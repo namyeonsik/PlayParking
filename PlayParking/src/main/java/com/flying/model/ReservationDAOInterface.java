5d7e9da0e1400d39b7ea59f8ef543291ca6c6d6d
insoo <insoo@203.249.75.82>
2015-02-09 09:45:47 +0900

    haha

---
+++ PlayParking/src/main/java/com/flying/model/ReservationDAOInterface.java
@@ -0,0 +1,8 @@
+package com.flying.model;
+
+import java.util.List;
+
+public interface ReservationDAOInterface {
+	public List<ReservationDTO> selectBymid(String mid);
+	public int insertReservation(ReservationDTO reserve);
+}
