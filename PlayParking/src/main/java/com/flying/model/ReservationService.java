79ff196278496e6cebcc978257dd6c1bc2b08202
dayea <admin@203.249.75.99>
2015-02-13 09:43:02 +0900

    reservationservice

--- PlayParking/src/main/java/com/flying/model/ReservationService.java
+++ PlayParking/src/main/java/com/flying/model/ReservationService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("reservationservice")
+@Component 
 public class ReservationService implements ReservationServiceInterface{
 
 	@Autowired
